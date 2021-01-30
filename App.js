85111c0f4891ce27d300c4e5a6b5141aab3994a0
Eduardo Yuji Matheus <eymath@hotmail.com>
2021-01-29 23:33:54 -0300

    multiple export

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
 import First from './components/FirstComponent'
-import ComponentExportDefault from './components/MultiExport'
-import ComponentExport1 from './components/MultiExport'
-import ComponentExport2 from './components/MultiExport'
+import QMS, {ComponentExport1, ComponentExport2}from './components/MultiExport'
+
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ComponentExportDefault/>
+      <QMS/>
       <ComponentExport1/>
       <ComponentExport2/>
       <First/>
